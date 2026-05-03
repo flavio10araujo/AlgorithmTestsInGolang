e16ee5fdd559e8541bd1d3d1b6a6323256df8b4a
Flavio Araujo <flavio10araujo@yahoo.com.br>
2026-05-02 22:06:28 -0400

    leetcode 703 (heap)

---
+++ data_structures_and_algo/leetcode/leet_703_kth_largest_element_in_a_stream/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "container/heap"
+
+type MinHeap []int
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+type KthLargest struct {
+	k    int
+	heap *MinHeap
+}
+
+func Constructor(k int, nums []int) KthLargest {
+	h := &MinHeap{}
+	heap.Init(h)
+
+	kth := KthLargest{
+		k:    k,
+		heap: h,
+	}
+
+	for _, n := range nums {
+		kth.Add(n)
+	}
+
+	return kth
+}
+
+func (this *KthLargest) Add(val int) int {
+	if this.heap.Len() < this.k {
+		heap.Push(this.heap, val)
+		return (*this.heap)[0]
+	}
+
+	kthLargest := (*this.heap)[0]
+
+	if val <= kthLargest {
+		return kthLargest
+	}
+
+	heap.Pop(this.heap)
+	heap.Push(this.heap, val)
+
+	return (*this.heap)[0]
+}
